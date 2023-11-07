61a7134a0f5c237d0bbe4149097b93a4e132a27b
MOksentuk <113674979+MOksentuk@users.noreply.github.com>
2023-11-07 20:53:05 +0300

    working not with complex input

--- Bones 2.0.py
+++ Bones 2.0.py
@@ -1,69 +1,79 @@
 class Manager:
+    liter_const = 1.1365
+    glass_const = 0.273
+    pint_const = 0.56826
+
     @staticmethod
     def checker(amount):
-        """Проверки превышения максимально допустимого значения"""
+        """Проверка превышения максимально допустимого значения"""
         check_const = 2560
-
-        return amount > check_const
-
-
+        if amount > check_const:
+            return True
 
     @staticmethod
-    def getter(value):
+    def summator(*objects):
+        ans = 0
+        for obj in objects: ans += obj.amount
+        return ans
+
+    @staticmethod
+    def decorator(value, name_1, name_2, name_3):
+        ans = ''
+        if 1 < abs(value % 10) < 5 or int(value) != value:
+            ans += f'{value} {name_1}'
+        elif abs(value) == 1:
+            ans += f'{value} {name_2}'
+        elif 4 < abs(value % 10) < 10 or value != 0:
+            ans += f'{value} {name_3}'
+        return ans
+
+    def returner(self, value):
         """Перевод из литров в начальные меры объёма"""
+        if value==0:
+            return 'Полученное значение 0'
         bushel_const = 32
         gallon_const = 4
         bushel = value // bushel_const
         gallon = (value % bushel_const) // gallon_const
         quart = value % bushel_const % gallon_const
-        ans = 'В полученном значении '
-        if 1 < bushel % 10 < 5:
-            ans += f'{bushel} бушеля'  # (1) бушель, (2-4) бушеля, (5-9) бушелей
-        elif bushel == 1:
-            ans += f'{bushel} бушель'
-        elif 4 < bushel % 10 < 10 or bushel != 0:
-            ans += f'{bushel} бушелей'
+        ans = ''
+        ans += self.decorator(bushel, 'бушеля', 'бушель', 'бушелей')
 
-        if bushel != 0 and gallon != 0: ans += ', '
+        if bushel != 0 and gallon != 0:
+            ans += ', '
+        ans += self.decorator(gallon, 'галлона', 'галлон', 'галлонов')
 
-        if 1 < gallon % 10 < 5:
-            ans += f'{gallon} галлона'  # (1) бушель, (2-4) бушеля, (5-9) бушелей
-        elif gallon == 1:
-            ans += f'{gallon} галлон'
-        elif 4 < gallon % 10 < 10 or gallon != 0:
-            ans += f'{gallon} галлонов'
-        if quart != 0 and gallon != 0: ans += ', '
-        if 1 < quart % 10 < 4 or int(quart) != quart:
-            ans += f'{quart} кварты'  # (1) бушель, (2-4) бушеля, (5-9) бушелей
-        elif quart == 1:
-            ans += f'{quart} кварта'
-        return ans + '.'
+        if quart != 0 and gallon != 0:
+            ans += ', '
+        ans += self.decorator(quart, 'кварты', 'кварта', 'кварт')
+
+        return ans
 
     def plus(self, lhs, rhs):
         """Сложение двух значений"""
         if self.checker(lhs.amount) or self.checker(rhs.amount):
-            raise ValueError("Переданное значение превышает максимально допустимое значение")
+            raise ValueError("Передано недопустимое значение")
         result = lhs.amount + rhs.amount
         if self.checker(result):
             raise ValueError("Полученное значение превышает максимально допустимое значение")
-        return self.getter(result)
+        return 'В полученном значении '+self.returner(result)
 
     def minus(self, lhs, rhs):
         """Вычитание двух значений"""
         if self.checker(lhs.amount) or self.checker(rhs.amount):
-            raise ValueError("Переданное значение превышает максимально допустимое значение")
+            raise ValueError("Передано недопустимое значение")
         result = lhs.amount - rhs.amount
-        return self.getter(result)
+        return 'В полученном значении '+self.returner(result)
 
     def comparison(self, lhs, rhs):
         """Сравнение двух значений"""
         if self.checker(lhs.amount) or self.checker(rhs.amount):
             raise ValueError("Переданное значение превышает максимально допустимое значение")
         result = abs(lhs.amount - rhs.amount)
-        if lhs > rhs:
-            return 'Первое значение больше второго на', result.getter
-        elif lhs < rhs:
-            return 'Первое значение меньше второго на', result.getter
+        if lhs.amount > rhs.amount:
+            return 'Первое значение больше второго на ' + self.returner(result)
+        elif lhs.amount < rhs.amount:
+            return 'Первое значение меньше второго на ' + self.returner(result)
         else:
             return 'Значения равны'
 
@@ -72,43 +82,46 @@ class Manager:
         if self.checker(obj.amount):
             raise ValueError("Переданное значение превышает максимально допустимое значение")
         result = obj.amount / const
-        return self.getter(result)
+        return 'В полученном значении '+self.returner(result)
 
     def multiplication_by_number(self, lhs, const):
         """Умножение на число"""
         result = lhs.amount * const
         if self.checker(result):
             raise ValueError("Полученное значение превышает максимально допустимое значение")
-        return self.getter(result)
+        return 'В полученном значении '+self.returner(result)
 
-    def addition_to_max_value(self, lhs, rhs):  
+    def addition_to_max_value(self, obj):
         """Дополнение до максимального значения"""
-        result = lhs.amount + rhs.amount
-        if self.checker(result):
+        if self.checker(obj.amount):
             raise ValueError("Полученное значение превышает максимально допустимое значение")
-        return self.getter(result)
+        max_value = 256
+        result = obj.amount - max_value
+        return 'В полученном значении '+self.returner(result)
 
     def to_liter(self, obj):
         """Возвращение значения в литрах"""
         if self.checker(obj.amount):
             raise ValueError("Полученное значение превышает максимально допустимое значение")
-        result = obj.amount * obj.liter_const
+        result = obj.amount * self.liter_const
         ans = 'В полученном значении '
-        return ans
+        return ans + self.decorator(result, 'литра', 'литр', 'литров')
 
     def to_glass(self, obj):
         """Перевод значения в стаканы"""
-        const = 0.273
         if self.checker(obj.amount):
             raise ValueError("Полученное значение превышает максимально допустимое значение")
-        return self.to_liter(obj) / const
+        result = obj.amount * self.liter_const / self.glass_const
+        ans = 'В полученном значении '
+        return ans + self.decorator(result, 'стакана', 'стакан', 'стаканов')
 
     def to_pint(self, obj):
         """Перевод значения в пинты"""
-        const = 0.56826
         if self.checker(obj.amount):
             raise ValueError("Полученное значение превышает максимально допустимое значение")
-        return self.to_liter(obj) / const
+        result = obj.amount * self.liter_const / self.pint_const
+        ans = 'В полученном значении '
+        return ans + self.decorator(result, 'пинты', 'пинта', 'пинт')
 
 
 class Quart:
@@ -119,7 +132,6 @@ class Quart:
 
 
 class Gallon:
-    liter_const = 4.546
     quart_const = 4
 
     def __init__(self, amount):
@@ -127,7 +139,6 @@ class Gallon:
 
 
 class Bushel:
-    liter_const = 36.368
     quart_const = 32
 
     def __init__(self, amount):
@@ -135,17 +146,19 @@ class Bushel:
 
 
 q1 = Quart(11)
-# g1 = Gallon(23)
+g1 = Gallon(2)
 q2 = Quart(16)
 c1 = Bushel(20)
 c2 = Bushel(3)
-
+c3 = Bushel(1)
 manager = Manager()
 print(manager.plus(q2, q1))
-# print(manager.plus(c2, c1))
-# print(manager.to_glass(q1))
-# print(manager.to_liter(q1))
+print(manager.minus(c2, c1))
+print(manager.comparison(c2, c1))
+print(manager.addition_to_max_value(c2))
+print(manager.to_glass(q1))
+print(manager.to_liter(q1))
 print(manager.division_by_number(q1, 4))
-print(manager.division_by_number(c1, 1))
-print(manager.division_by_number(q1, 11))
-print(manager.multiplication_by_number(q1, 2))
+print(manager.multiplication_by_number(c1, 4))
+print(manager.multiplication_by_number(q1, 0))
+# print(manager.multiplication_by_number(manager.summator(q1, g1, c2), 2)
