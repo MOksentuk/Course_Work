d236d4bbe4fcf6fcc78915d4f2ce95bc7c513688
MOksentuk <113674979+MOksentuk@users.noreply.github.com>
2023-11-08 17:17:12 +0300

    separating operations into files

--- project/Manager.py
+++ project/Manager.py
@@ -1,7 +1,4 @@
 class Manager:
-    __liter_const = 1.1365
-    __glass_const = 0.273
-    __pint_const = 0.56826
 
     @staticmethod
     def checker(*objects):
@@ -9,7 +6,7 @@ class Manager:
         check_const = 2560
 
         for obj in objects:
-            if obj.amount > check_const:
+            if obj > check_const:
                 raise ValueError("Передано недопустимое значение")
 
     @staticmethod
@@ -53,77 +50,3 @@ class Manager:
         ans += self.decorator(quart, 'кварты', 'кварта', 'кварт')
 
         return ans
-
-    def plus(self, lhs, rhs):
-        """Сложение двух значений"""
-        self.checker(lhs.amount, rhs.amount)
-
-        result = lhs.amount + rhs.amount
-        if self.checker(result):
-            raise ValueError("Полученное значение превышает максимально допустимое значение")
-        return 'В полученном значении ' + self.returner(result)
-
-    def minus(self, lhs, rhs):
-        """Вычитание двух значений"""
-        self.checker(lhs, rhs)
-
-        result = lhs.amount - rhs.amount
-        return 'В полученном значении ' + self.returner(result)
-
-    def comparison(self, lhs, rhs):
-        """Сравнение двух значений"""
-        if self.checker(lhs.amount) or self.checker(rhs.amount):
-            raise ValueError("Переданное значение превышает максимально допустимое значение")
-        result = abs(lhs.amount - rhs.amount)
-        if lhs.amount > rhs.amount:
-            return 'Первое значение больше второго на ' + self.returner(result)
-        elif lhs.amount < rhs.amount:
-            return 'Первое значение меньше второго на ' + self.returner(result)
-        else:
-            return 'Значения равны'
-
-    def division_by_number(self, obj, const):
-        """Деление на число"""
-        if self.checker(obj.amount):
-            raise ValueError("Переданное значение превышает максимально допустимое значение")
-        result = obj.amount / const
-        return 'В полученном значении ' + self.returner(result)
-
-    def multiplication_by_number(self, lhs, const):
-        """Умножение на число"""
-        result = lhs.amount * const
-        if self.checker(result):
-            raise ValueError("Полученное значение превышает максимально допустимое значение")
-        return 'В полученном значении ' + self.returner(result)
-
-    def addition_to_max_value(self, obj):
-        """Дополнение до максимального значения"""
-        if self.checker(obj.amount):
-            raise ValueError("Полученное значение превышает максимально допустимое значение")
-        max_value = 256
-        result = obj.amount - max_value
-        return 'В полученном значении ' + self.returner(result)
-
-    def to_liter(self, obj):
-        """Возвращение значения в литрах"""
-        if self.checker(obj.amount):
-            raise ValueError("Полученное значение превышает максимально допустимое значение")
-        result = obj.amount * self.__liter_const
-        ans = 'В полученном значении '
-        return ans + self.decorator(result, 'литра', 'литр', 'литров')
-
-    def to_glass(self, obj):
-        """Перевод значения в стаканы"""
-        if self.checker(obj.amount):
-            raise ValueError("Полученное значение превышает максимально допустимое значение")
-        result = obj.amount * self.__liter_const / self.__glass_const
-        ans = 'В полученном значении '
-        return ans + self.decorator(result, 'стакана', 'стакан', 'стаканов')
-
-    def to_pint(self, obj):
-        """Перевод значения в пинты"""
-        if self.checker(obj.amount):
-            raise ValueError("Полученное значение превышает максимально допустимое значение")
-        result = obj.amount * self.__liter_const / self.__pint_const
-        ans = 'В полученном значении '
-        return ans + self.decorator(result, 'пинты', 'пинта', 'пинт')
