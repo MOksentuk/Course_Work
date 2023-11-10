8b5110d40071a9c72aa9702907935fffc5b8cf1f
MOksentuk <113674979+MOksentuk@users.noreply.github.com>
2023-11-10 10:07:33 +0300

    Operations completed

--- project/Operations/MathOperations.py
+++ project/Operations/MathOperations.py
@@ -1,50 +1,44 @@
 class MathOperations:
-    def plus(self, lhs, rhs):
+    @staticmethod
+    def plus(lhs, rhs):
         """Сложение двух значений"""
-        self.checker(lhs.amount, rhs.amount)
 
-        result = lhs.amount + rhs.amount
-        if self.checker(result):
-            raise ValueError("Полученное значение превышает максимально допустимое значение")
-        return 'В полученном значении ' + self.returner(result)
-
-    def minus(self, lhs, rhs):
-        """Вычитание двух значений"""
-        self.checker(lhs, rhs)
-
-        result = lhs.amount - rhs.amount
+        result = lhs + rhs
         return result
 
-    def comparison(self, lhs, rhs):
+    @staticmethod
+    def minus(lhs, rhs):
+        """Вычитание двух значений"""
+
+        result = lhs - rhs
+        return result
+
+    @staticmethod
+    def comparison(lhs, rhs):
         """Сравнение двух значений"""
-        if self.checker(lhs.amount) or self.checker(rhs.amount):
-            raise ValueError("Переданное значение превышает максимально допустимое значение")
         result = abs(lhs.amount - rhs.amount)
         if lhs.amount > rhs.amount:
-            return 'Первое значение больше второго на ' + self.returner(result)
+            return 1, result
         elif lhs.amount < rhs.amount:
-            return 'Первое значение меньше второго на ' + self.returner(result)
+            return -1, result
         else:
-            return 'Значения равны'
+            return 0
 
-    def division_by_number(self, obj, const):
+    @staticmethod
+    def division_by_number(amount, const):
         """Деление на число"""
-        if self.checker(obj.amount):
-            raise ValueError("Переданное значение превышает максимально допустимое значение")
-        result = obj.amount / const
-        return 'В полученном значении ' + self.returner(result)
+        result = amount / const
+        return result
 
-    def multiplication_by_number(self, lhs, const):
+    @staticmethod
+    def multiplication_by_number(amount, const):
         """Умножение на число"""
-        result = lhs.amount * const
-        if self.checker(result):
-            raise ValueError("Полученное значение превышает максимально допустимое значение")
-        return 'В полученном значении ' + self.returner(result)
+        result = amount * const
+        return result
 
-    def addition_to_max_value(self, obj):
+    @staticmethod
+    def addition_to_max_value(amount):
         """Дополнение до максимального значения"""
-        if self.checker(obj.amount):
-            raise ValueError("Полученное значение превышает максимально допустимое значение")
         max_value = 256
-        result = obj.amount - max_value
-        return 'В полученном значении ' + self.returner(result)
+        result = amount - max_value
+        return result
