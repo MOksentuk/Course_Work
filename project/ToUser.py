92716c6bb9be720d63539385ea16e50b5f0d10bd
Konstantin <112973356+PchyolkaZloy@users.noreply.github.com>
2023-11-21 19:17:02 +0300

    Update ToUser.py

--- project/ToUser.py
+++ project/ToUser.py
@@ -8,22 +8,25 @@ from Operations.Math import MathOperations
 
 
 class ToUser:
-    manager = Manager()
-  
+    def __init__(self, manager):
+        _manager = manager;
+
+    
     def init(self, name):
         amount = input(f'{name}: ')
 
-        self.manager.check_is_digit(amount)
-        self.manager.check_max_value(amount)
+        self._manager.check_is_digit(amount)
+        self._manager.check_max_value(amount)
 
         return amount
 
+    
     def comparison_processing(self, ans):
         label = ans[0]
         amount = ans[1]
         if label:
-            return f'Первое значение больше второго на {self.manager.returner(amount)}'
+            return f'Первое значение больше второго на {self._manager.returner(amount)}'
         elif not label:
-            return f'Первое значение меньше второго на {self.manager.returner(amount)}'
+            return f'Первое значение меньше второго на {self._manager.returner(amount)}'
         else:
             return 'Значения равны'
